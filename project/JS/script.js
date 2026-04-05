86ad40d0b2fb0a728655f8474c3f8502644c8744
Gerald <g.redl@students.htl-leonding.ac.at>
2026-04-05 22:42:55 +0200

    Options - 2

--- project/JS/script.js
+++ project/JS/script.js
@@ -1,3 +1,27 @@
+let earth = document.getElementById('RotatingEarth');
+let moon = document.getElementById('RotatingMoon');
+
+function movePlanets(element) {
+
+    if(element == 'Homepage') {
+        earth.style.height = '90%'
+        earth.style.top = '5%'
+        earth.style.right = '10%'
+
+        moon.style.height = '10%'
+        moon.style.top = '5%'
+        moon.style.left = '3%' 
+    } else if(element == 'Options') {
+        earth.style.height = '40%'
+        earth.style.top = '60%'
+        earth.style.right = '0%'
+
+        moon.style.height = '15%'
+        moon.style.top = '2%'
+        moon.style.left = '40%' 
+    }
+
+}
 
 function showNextScreen(currentElement, nextElement) {
     let currentE = document.getElementById(currentElement)
@@ -5,4 +29,6 @@ function showNextScreen(currentElement, nextElement) {
 
     currentE.style.visibility = "hidden"
     nextE.style.visibility = "visible"
+
+    movePlanets(nextElement)
 }
