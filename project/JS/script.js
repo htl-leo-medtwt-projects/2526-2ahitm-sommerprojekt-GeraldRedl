42ba07d2c0cc34bc5159411270e09543452be3e3
Gerald <g.redl@students.htl-leonding.ac.at>
2026-04-05 21:38:27 +0200

    Options - 1

--- project/JS/script.js
+++ project/JS/script.js
@@ -1,4 +1,8 @@
 
 function showNextScreen(currentElement, nextElement) {
+    let currentE = document.getElementById(currentElement)
+    let nextE = document.getElementById(nextElement)
 
+    currentE.style.visibility = "hidden"
+    nextE.style.visibility = "visible"
 }
