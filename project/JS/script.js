2bbc5ed859a97bf95e5222c21132da2e86de3942
Gerald <g.redl@students.htl-leonding.ac.at>
2026-04-06 15:48:52 +0200

    GameMode - 1

--- project/JS/script.js
+++ project/JS/script.js
@@ -1,9 +1,9 @@
 let earth = document.getElementById('RotatingEarth');
 let moon = document.getElementById('RotatingMoon');
 
-function movePlanets(element) {
+function movePlanets(element) { // Bewegung der Planeten bei Bildschirmwechsel
 
-    if(element == 'Homepage') {
+    if(element == 'Homepage') { // Bewegung zur Homepage
         earth.style.height = '90%'
         earth.style.top = '5%'
         earth.style.right = '10%'
@@ -11,7 +11,7 @@ function movePlanets(element) {
         moon.style.height = '10%'
         moon.style.top = '5%'
         moon.style.left = '3%' 
-    } else if(element == 'Options') {
+    } else if(element == 'Options') { // Bewegung zu den Optionen
         earth.style.height = '40%'
         earth.style.top = '60%'
         earth.style.right = '0%'
@@ -19,7 +19,7 @@ function movePlanets(element) {
         moon.style.height = '15%'
         moon.style.top = '2%'
         moon.style.left = '40%' 
-    } else if(element == 'Rules') {
+    } else if(element == 'Rules') { // Bewegung zu den Regeln
         earth.style.height = '40%'
         earth.style.top = '0%'
         earth.style.right = '7%'
@@ -27,11 +27,19 @@ function movePlanets(element) {
         moon.style.height = '15%'
         moon.style.top = '70%'
         moon.style.left = '25%' 
+    } else if(element == 'GameMode') { // Bewegung zum Moduswechsel
+        earth.style.height = '70%'
+        earth.style.top = '15%'
+        earth.style.right = '32%'
+
+        moon.style.height = '15%'
+        moon.style.top = '7%'
+        moon.style.left = '-3%' 
     }
 
 }
 
-function showNextScreen(currentElement, nextElement) {
+function showNextScreen(currentElement, nextElement) { // Bildschirmwechsel
     let currentE = document.getElementById(currentElement)
     let nextE = document.getElementById(nextElement)
 
@@ -47,11 +55,12 @@ function showNextScreen(currentElement, nextElement) {
 **********************************************************
 *********************************************************/
 
+let rulesScreen = document.getElementById('Rules')
 let boxes = document.querySelectorAll(".Box");
 let current = 0;
 let isAnimating = false;
 
-function updateBoxes() {
+function updateBoxes() { // Bewegung der Boxen nach Oben/Unten
     boxes.forEach((box, i) => {
         box.style.transform = `translateY(${(i - current) * 100}vh)`;
     });
@@ -59,10 +68,10 @@ function updateBoxes() {
 
 updateBoxes();
 
-window.addEventListener("wheel", (e) => {
-    if (isAnimating) return;
+window.addEventListener("wheel", (e) => { // EvenListener für das Wheel
+    if (isAnimating || rulesScreen.style.visibility == 'hidden') return; // Rauswurf während Animationen/inaktivem Bildschirm
 
-    if (e.deltaY > 0 && current < boxes.length - 1) {
+    if (e.deltaY > 0 && current < boxes.length - 1) { // Berechnung der Richtung des Scroll Wheels
         current++;
     } else if (e.deltaY < 0 && current > 0) {
         current--;
@@ -73,7 +82,15 @@ window.addEventListener("wheel", (e) => {
     isAnimating = true;
     updateBoxes();
 
-    setTimeout(() => {
+    setTimeout(() => { // Zurücksetzen des Timers für Animation
         isAnimating = false;
     }, 600);
 });
+
+/*********************************************************
+**********************************************************
+************************GAMEMODE**************************
+**********************************************************
+*********************************************************/
+
+let mapImages = {}
