ab5162ad01314fc78323b4649aecd48fd3b02f67
Gerald <g.redl@students.htl-leonding.ac.at>
2026-04-06 17:42:12 +0200

    GameMode - 2

--- project/JS/script.js
+++ project/JS/script.js
@@ -93,4 +93,51 @@ window.addEventListener("wheel", (e) => { // EvenListener für das Wheel
 **********************************************************
 *********************************************************/
 
-let mapImages = {}
+let mapImages = ['./Img/Europe.png', './Img/Asia.png', './Img/Africa.png', './Img/NorthAmerica.png', './Img/SouthAmerica.png', './Img/Australia.png']
+let worldMapGenerateTimer;
+
+function generateMaps() { // Generiert die einzelnen Maps für die Auswahl
+    worldMapGenerateTimer = setInterval(function() { // Generiert die World Map
+        let randomImage = Math.floor(Math.random()*mapImages.length)
+        document.querySelector('.MapWorld').innerHTML = `<img src="${mapImages[randomImage]}" alt="${mapImages[randomImage]}">`
+    }, 1000)
+
+    document.querySelector('.MapEurope').innerHTML = `<img src="${mapImages[0]}" alt="${mapImages[0]}">`
+    document.querySelector('.MapAsia').innerHTML = `<img src="${mapImages[1]}" alt="${mapImages[1]}">`
+    document.querySelector('.MapAfrica').innerHTML = `<img src="${mapImages[2]}" alt="${mapImages[2]}">`
+    document.querySelector('.MapNorthAmerica').innerHTML = `<img src="${mapImages[3]}" alt="${mapImages[3]}">`
+    document.querySelector('.MapSouthAmerica').innerHTML = `<img src="${mapImages[4]}" alt="${mapImages[4]}">`
+    document.querySelector('.MapAustralia').innerHTML = `<img src="${mapImages[5]}" alt="${mapImages[5]}">`
+    document.querySelector('.MapSuprise').innerHTML = `<img src="./IMG/Suprise.png" alt="Suprise.png">`
+}
+
+generateMaps()
+
+let alreadyRotated = false;
+let rotatedElement = -1;
+function generateGameModeOptions(element, index) { // Generiert per OnClick die jewiligen Optionen zum auswählen
+    if(index != rotatedElement) {
+        if(alreadyRotated) { // Dreht die Boxen in ihre Originalposition
+            let allMaps = document.querySelectorAll('.GameModeMaps')
+            allMaps.forEach((GameModeMaps, i) => {
+                GameModeMaps.style.transform = 'rotateY(0deg)';
+            });
+            alreadyRotated = false;
+        }
+
+        element.style.transform = 'rotateY(180deg)' // Dreht die ausgewählte Box um 180deg
+        alreadyRotated = true;
+        rotatedElement = index;
+
+        element.innerHTML += 
+            `
+            <div id="GameModeOptions">
+                <p>Hallo</p> 
+                <p>Hallo</p>
+                <p>Hallo</p>
+                <p>Hallo</p>
+            </div>
+            `
+        
+    }
+}
